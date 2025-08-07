480fb8ddbf7219d3eb77bfab7c19c5691ab94dff
niraj1076 <81097516+niraj1076@users.noreply.github.com>
2025-08-07 16:03:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
   
 var app = express();//Respond with "hello world" for requests that hit our root "/" 
 app.get('/', function (req, res) { 
- res.send('hello deployed it from the node app '); 
+ res.send('hello this is deployed from the node app jenkins pipeline'); 
 });//listen to port 3000 by default 
 app.listen(process.env.PORT || 3000); 
   
